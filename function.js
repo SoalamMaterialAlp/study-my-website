fdb34a12d20b2d49bf88e9a747ee39348ec25e09
SoalamMaterialAlp <alamkadir9256@gmail.com>
2026-09-13 23:11:01 +0530

    Delete function.js

--- function.js
+++
@@ -1,9 +0,0 @@
-// only function write 
-
-// function to copy the code 
-export function copycode(){
-    var codes = document.getElementById("program").innerText;
-    navigator.clipboard.writeText(codes);
-    alert("Code Copied!");
-}
-// export { copycode };
